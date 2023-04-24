92e19076e9075dcdc6eac2a571031814a47ba05d
Ivan-Camilo <ivan.rivera.s@gmail.com>
2023-04-24 00:31:54 -0500

    Second Commit

--- Examen-Moviles/Examen-Moviles/App.xaml.cs
+++ Examen-Moviles/Examen-Moviles/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExamenMoviles;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Examen_Moviles
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Login();
         }
 
         protected override void OnStart ()
