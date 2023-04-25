6bb0c4585545034facdc33c6f27e6cd536977cb9
Ivan-Camilo <ivan.rivera.s@gmail.com>
2023-04-24 23:44:24 -0500

    Tercer Commit

---
+++ Examen-Moviles/Examen-Moviles/Registro3.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ExamenMoviles
+{	
+	public partial class Registro3 : ContentPage
+	{	
+		public Registro3 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
